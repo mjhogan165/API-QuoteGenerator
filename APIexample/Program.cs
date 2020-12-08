c04af73ef4052b5ead023ab34abfef72b1808118
Matthew Hogan <mjhogan165@gmail.com>
2020-12-08 16:09:48 -0500

    Ron and Kanye talk

---
+++ APIexample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+        }
+    }
+}
---
+++ APIexample/QuoteGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace APIexample
+{
+    class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient(); //a new instance of the HttpClient class which provides a base class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.
+
+            var kanyeURL = "https://api.kanye.rest"; //string to hold KanyeURL
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result; //response from API as string
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString(); //
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+            Console.WriteLine("-------------------");
+
+        }
+        public static void RonQuote()
+        {
+          
+
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine($"Ron: {ronQuote}");
+            Console.WriteLine("-------------------");
+        }
+
+    }
+}
